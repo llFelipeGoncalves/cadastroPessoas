1ac8ba5eb0cac6346211055882e673d161495d5f
llFelipeGoncalves <luizfelipe.goncalves442@gmail.com>
2026-05-03 03:24:13 -0300

    feat: refactor Main and UserController for improved user interaction and service integration

--- src/main/java/oi/github/llFelipeGoncalves/Main.java
+++ src/main/java/oi/github/llFelipeGoncalves/Main.java
@@ -1,114 +1,20 @@
 package oi.github.llFelipeGoncalves;
 
+import oi.github.llFelipeGoncalves.controllers.UserController;
 import oi.github.llFelipeGoncalves.dao.UserDAO;
-import oi.github.llFelipeGoncalves.exceptions.EmptyStorageException;
-import oi.github.llFelipeGoncalves.exceptions.UserNotFoundException;
-import oi.github.llFelipeGoncalves.exceptions.ValidatorException;
-import oi.github.llFelipeGoncalves.models.MenuOptions;
-import oi.github.llFelipeGoncalves.models.UserModel;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.List;
-import java.util.Scanner;
-
-import static oi.github.llFelipeGoncalves.validator.UserValidator.parseAndValidateBirthday;
-import static oi.github.llFelipeGoncalves.validator.UserValidator.verifyModel;
+import oi.github.llFelipeGoncalves.service.UserServiceImpl;
+import oi.github.llFelipeGoncalves.views.ConsoleViews;
 
 public class Main {
-    private static final UserDAO dao = new UserDAO();
-    private static final Scanner scanner = new Scanner(System.in);
-
      public static void main(String[] args) {
+        UserDAO userDAO = new UserDAO();
+        ConsoleViews consoleViews = new ConsoleViews();
+        UserServiceImpl uServiceImpl = new UserServiceImpl(userDAO);
+        UserController userController = new UserController(uServiceImpl, consoleViews);
 
         while (true) {
-            
-            int userInput = scanner.nextInt();
-            MenuOptions selectOption = MenuOptions.values()[userInput - 1];
-            switch (selectOption) {
-                case SAVE -> {
-                    try {
-                        UserModel user = dao.save(requestToSave());
-                        System.out.printf("Usuário salvo... %s", user);
-                    } catch (ValidatorException | DateTimeParseException e) {
-                        System.err.println(e.getMessage());
-                    }
-                }
-                case UPDATE -> {
-                    try {
-                        UserModel user = dao.update(requestToUpdate());
-                        System.out.printf("Usuário salvo... %s", user);
-                    } catch (UserNotFoundException | EmptyStorageException | ValidatorException | DateTimeParseException e) {
-                        System.err.println(e.getMessage());
-                    }
-                }
-                case DELETE -> {
-                    try {
-                        dao.delete(requestId());
-                        System.out.println("Usuário deletado...");
-                    } catch (UserNotFoundException | EmptyStorageException e) {
-                        System.err.println(e.getMessage());
-                    }
-                }
-                case FIND_BY_ID -> {
-                    try {
-                        var id = requestId();
-                        var user = dao.findById(id);
-                        System.out.printf("Usuário com id %s", id);
-                        System.out.println(user);
-                    } catch (UserNotFoundException | EmptyStorageException e) {
-                        System.err.println(e.getMessage());
-                    }
-                }
-
-                case FIND_ALL -> {
-                    List<UserModel> users = dao.findAll();
-                    System.out.println("Usuários cadastrados");
-                    users.forEach(System.out::println);
-                }
-                case EXIT -> System.exit(0);
-            }
+            consoleViews.display();
+            userController.handleMenuOptions(consoleViews.getMenuOptions());
         }
-
-    }
-
-    private static long requestId() {
-        System.out.println("informe o identificador do usuário >_: ");
-        return scanner.nextLong();
-    }
-
-    private static UserModel requestToSave() throws ValidatorException {
-        System.out.println("informe o nome do usuário");
-        String name = scanner.next();
-        System.out.println("informe o e-mail do usuário");
-        String email = scanner.next();
-        System.out.println("informe a data de aniversário do usuário");
-        String birthdayString = scanner.next();
-        LocalDate birthday = parseAndValidateBirthday(birthdayString);
-
-        return validadeInputs(0, name, email, birthday);
-
-    }
-
-    private static UserModel validadeInputs(final long id, final String name,
-                                              final String email, final LocalDate birthday) throws ValidatorException {
-         UserModel user = new UserModel(0, name, email, birthday);
-         verifyModel(user);
-         return user;
-    }
-
-    private static UserModel requestToUpdate() throws ValidatorException {
-        System.out.println("informe o identificador do usuário >_: ");
-        long id = scanner.nextLong();
-        System.out.println("informe o nome do usuário >_: ");
-        String name = scanner.next();
-        System.out.println("informe o e-mail do usuário >_: ");
-        String email = scanner.next();
-        System.out.println("informe a data de aniversário do usuário >_: ");
-        String birthdayString = scanner.next();
-        LocalDate birthday = parseAndValidateBirthday(birthdayString);
-
-        return validadeInputs(id, name, email, birthday);
-    }
-
+     }
 }
--- src/main/java/oi/github/llFelipeGoncalves/controllers/UserController.java
+++ src/main/java/oi/github/llFelipeGoncalves/controllers/UserController.java
@@ -1,7 +1,17 @@
 package oi.github.llFelipeGoncalves.controllers;
 
+import static oi.github.llFelipeGoncalves.validator.UserValidator.parseAndValidateBirthday;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+import oi.github.llFelipeGoncalves.exceptions.UserNotFoundException;
+import oi.github.llFelipeGoncalves.exceptions.ValidatorException;
 import oi.github.llFelipeGoncalves.models.MenuOptions;
+import oi.github.llFelipeGoncalves.models.UserModel;
 import oi.github.llFelipeGoncalves.service.IUserService;
+import oi.github.llFelipeGoncalves.service.UserServiceImpl;
 import oi.github.llFelipeGoncalves.views.ConsoleViews;
 
 public class UserController {
@@ -11,22 +21,70 @@ public class UserController {
 
   public UserController(IUserService userService, ConsoleViews consoleViews) {
     this.userService = userService;
-    this.consoleViews = consoleViews; 
+    this.consoleViews = consoleViews;
   }
 
   public void handleMenuOptions(MenuOptions option) {
     switch (option) {
-      case SAVE -> 
-      case UPDATE -> 
-      case DELETE -> 
-      case FIND_BY_ID -> 
-      case FIND_ALL -> 
-      case INVALID -> 
+      case SAVE -> handleSave();
+      case UPDATE -> handleUpdate();
+      case DELETE -> handleDelete();
+      case FIND_BY_ID -> handleFindById();
+      case FIND_ALL -> handleFindAll();
+      case INVALID -> consoleViews.displayError("opcao invalida. tente novamente...");
     }
   }
 
   public void handleSave() {
-    
+    try {
+      String name = consoleViews.requestName();
+      String email = consoleViews.requestEmail();
+      String birthdayString = consoleViews.requestBirthday();
+      LocalDate birthday = parseAndValidateBirthday(birthdayString);
+      UserModel user = userService.createUser(name, email, birthday);
+      consoleViews.displayMessage("Usuario salvo" + user);
+    } catch (ValidatorException | DateTimeParseException e) {
+      consoleViews.displayError(e.getMessage());
+    }
+  }
+
+  public void handleUpdate() {
+    try {
+      long id = consoleViews.requestId();
+      String name = consoleViews.requestName();
+      String email = consoleViews.requestEmail();
+      String birthdayString = consoleViews.requestBirthday();
+      LocalDate birthday = parseAndValidateBirthday(birthdayString);
+      UserModel user = userService.updateUser(id, name, email, birthday);
+      consoleViews.displayMessage("Usuario atualizado" + user);
+    } catch (UserNotFoundException | ValidatorException | DateTimeParseException e) {
+      consoleViews.displayError(e.getMessage());
+    }
+  }
+
+  public void handleDelete() {
+    try {
+      long id = consoleViews.requestId();
+      userService.deleteUser(id);
+      consoleViews.displayMessage("usuario deletado");
+    } catch (UserNotFoundException e) {
+      consoleViews.displayError(e.getMessage());
+    }
+  }
+
+  public void handleFindById() {
+    try {
+      long id = consoleViews.requestId();
+      UserModel user = userService.findById(id);
+      consoleViews.displayUser(user);
+    } catch (UserNotFoundException e) {
+      consoleViews.displayError(e.getMessage());
+    }
+  }
+
+  public void handleFindAll() {
+    List<UserModel> users = userService.findAll();
+    consoleViews.displayUsers(users);
   }
 
 }
